4ece0d36b7a5e0bc7a8b26656470b9fbc6b046ad
wiscer <wiscer.org@gmail.com>
2025-07-11 14:41:49 +1000

    Globally declare PageLive type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,16 +3,25 @@
 
 // Global type declaration for pageLiveAnnounce on the Window interface
 declare global {
+    /**
+     * Type definitions for the PageLive extension.
+     * This file contains global type declarations for the PageLive functionality.
+     * It is used to ensure that the TypeScript compiler recognizes the global `pageLive` object on the `Window` interface.
+     */
+
+    type PageLive = {
+        PAGE_LIVE_CONTAINER_ID: string;
+        ANNOUNCE_CONTAINER_ID: string;
+        ensurePageLiveContainer: () => void;
+        announce: (announceObj: { msg: string }) => void;
+        init: () => void;
+    };
+    
+    // Extend the Window interface to include the pageLive object
     interface Window {
         // Note: The `pageLive` object is used as shared global object for PageLive functionality. 
         // This will be ran under separate JS context in content scripts, so do not need to worry about conflicts with scripts of the page.
-        pageLive: {
-            PAGE_LIVE_CONTAINER_ID: string;
-            ANNOUNCE_CONTAINER_ID: string;
-            ensurePageLiveContainer: () => void;
-            announce: (announceObj: { msg: string }) => void;
-            init: () => void;
-        };
+        pageLive: PagLive,
     }
 }
 
