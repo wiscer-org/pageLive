d60a133dea02d54d7728ff2849bcf14e496e8cf2
wiscer <wiscer.org@gmail.com>
2025-09-25 16:48:37 +1000

    Gemini: Add notes to the chat container class

--- src/content-scripts/page-adapters/gemini-chat.ts
+++ src/content-scripts/page-adapters/gemini-chat.ts
@@ -1,25 +1,3 @@
-
-// Below a part of the hierarchy of chat container to used to parse a response in this file :
-//div#chat-history
-//	infinite-scroller [data-test-id="chat-history-container"]
-//		div.conversation-container
-//	    div.conversation-container
-//			user-query
-//			model-response
-//				div
-//					response-container
-//						div.response-container
-//							div.response-container-header
-//								div.response-container-header-controls
-//								div.response-container-header-status
-//							div.presented-response-container
-//								div.avatar-gutter
-//								div.response-container-content
-//									div.response-content
-//										message-content#message-content-id-r_d1657b4a1ad3e5b2.model-response-text
-
-
-
 import { devLog, prodWarn, untilElementIdle, waitForAnElement } from "../util";
 /**
  * This class features interaction with the chat container element of gemini page.
@@ -232,3 +210,53 @@ export class GeminiAdapterChat {
     }
 
 }
+
+
+// Below is note about how to announce an incoming new response:
+// 
+// Problem:
+// An incoming new response is not fully rendered, but in streams. 
+// The previous solution is to wait until not more mutations happened in the 'chat container', which will make the user's wait time is longer before each new response is read.
+// 
+// Objective:
+// Announce each response segment right away once it fully rendered. For the last segment, it will be announced with a delay after no more mutations happened.
+// 
+// Condition:
+// A 'response segment' is all first children element of a response element, e.g.: <p>, <ul>, etc.
+// A 'response element' is the direct parent of `responseSegments` of a response. Basically it only contains the whole text received as a response.
+// A 'response element' could be queried with selector 'message-content'.
+// A 'response container' is an HTML element that wraps all elements related to a response, including `responseElement`, buttons, etc.
+// A 'response container' could be 'catch' when the element is added to the `chatContainer` element.
+//
+// Strategy:
+// - Observe the `chatContainer` element to catch if a new `responseContainer` is added by mutation.
+//  `responseContainer` does not have to be a specific element. It just has to contain the new 'response element'.
+// - After `responseContainer` is caught, query for `responseElement` on every mutations.
+// The reason for querying instead of 'catching' the node, is that the `respone element` does not have unique tag name, id, or classes to be used to catch the element.
+// - After `responseElement` is exits. Start observing the `responseElement` and stop observing `chatContainer`.
+// The reason to start another observer, the one that focused on `responseElement`, is for performance reason. 
+// So we do not need to count whether a segment has beend added or not on every mutations. Consider there will be a lot of element mutations happenning during receiving new response.
+// - Every time a segment 'n' is added, announce right away the segments before the last one, schedule to announce the segment 'n' after some delay, and cancel the previously set timeout.
+// We can set the delay a bit long, like 4 seconds. It will not increase user's wait time since screen reader need to read the earlier segments first. 
+// It will only effect response that has only 1 segment.
+
+
+// Below is a part of the hierarchy of chat container to used to parse a response in this file :
+//div#chat-history
+//	infinite-scroller [data-test-id="chat-history-container"]
+//		div.conversation-container
+//	    div.conversation-container
+//			user-query
+//			model-response
+//				div
+//					response-container
+//						div.response-container
+//							div.response-container-header
+//								div.response-container-header-controls
+//								div.response-container-header-status
+//							div.presented-response-container
+//								div.avatar-gutter
+//								div.response-container-content
+//									div.response-content
+//										message-content#message-content-id-r_d1657b4a1ad3e5b2.model-response-text
+
